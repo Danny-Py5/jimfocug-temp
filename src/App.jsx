54a30ac043172db28cbfbebc877d7336692c719d
Olatunde Daniel Fatokun <olatundedaniel943@gmail.com>
2026-01-16 21:39:10 +0100

    resolve merge comflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 import { Outlet } from "react-router-dom";
 import { Header } from "./components/Header.jsx";
-=======
-import "./App.css";
->>>>>>> 7c07d5b97c0e9496a0eb2b1eec5bc47113644baa
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Header />
       <Outlet />
-=======
-      <h1>Welcome to Jimfocug</h1>
->>>>>>> 7c07d5b97c0e9496a0eb2b1eec5bc47113644baa
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
@@ -25,15 +24,3 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
->>>>>>> 7c07d5b97c0e9496a0eb2b1eec5bc47113644baa
